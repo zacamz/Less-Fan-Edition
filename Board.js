01430327cc3f095bcf00e3b7812d808c64d2ed0e
Zachary Johnson <vepped@gmail.com>
2023-06-01 15:24:32 -0400

    Completely Rendered the Board
    With the help of the almighty SAM we are progressing on this conquest to bring less to more. Currently created functions to render the board to the screen and next steps will be to add functionality to the JS and eventually get it on to a server so that we can take ownership over someone else game jk jk don't sue. we are fans.
    
    Co-Authored-By: kemptosa <99417853+kemptosa@users.noreply.github.com>

--- Board.js
+++ Board.js
@@ -18,48 +18,48 @@ let legend = {
     S: "space",
 }
 
-let coasters = 
-[[["D","U","D","U","D"],
-["W","S","U","S","U"],
-["D","U","D","U","D"],
-["W","S","U","S","U"],
-["D","U","D","U","D"]],
+let coasters =
+    [[["D", "U", "D", "U", "D"],
+    ["W", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"],
+    ["W", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"]],
 
-[["D","U","D","U","D"],
-["U","S","U","S","U"],
-["D","U","D","W","D"],
-["U","S","W","S","U"],
-["D","U","D","U","D"]],
+    [["D", "U", "D", "U", "D"],
+    ["U", "S", "U", "S", "U"],
+    ["D", "U", "D", "W", "D"],
+    ["U", "S", "W", "S", "U"],
+    ["D", "U", "D", "U", "D"]],
 
-[["D","U","D","U","D"],
-["U","S","U","S","U"],
-["D","U","D","U","D"],
-["U","S","W","S","U"],
-["D","W","D","U","D"]],
+    [["D", "U", "D", "U", "D"],
+    ["U", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"],
+    ["U", "S", "W", "S", "U"],
+    ["D", "W", "D", "U", "D"]],
 
-[["D","U","D","U","D"],
-["U","S","U","S","U"],
-["D","U","D","U","D"],
-["U","S","W","S","U"],
-["D","U","D","W","D"]],
+    [["D", "U", "D", "U", "D"],
+    ["U", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"],
+    ["U", "S", "W", "S", "U"],
+    ["D", "U", "D", "W", "D"]],
 
-[["D","U","D","U","D"],
-["U","S","W","S","U"],
-["D","U","D","W","D"],
-["U","S","U","S","W"],
-["D","U","D","U","D"]],
+    [["D", "U", "D", "U", "D"],
+    ["U", "S", "W", "S", "U"],
+    ["D", "U", "D", "W", "D"],
+    ["U", "S", "U", "S", "W"],
+    ["D", "U", "D", "U", "D"]],
 
-[["D","U","D","U","D"],
-["W","S","U","S","U"],
-["D","W","D","U","D"],
-["W","S","U","S","U"],
-["D","U","D","U","D"]],
+    [["D", "U", "D", "U", "D"],
+    ["W", "S", "U", "S", "U"],
+    ["D", "W", "D", "U", "D"],
+    ["W", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"]],
 
-[["D","U","D","U","D"],
-["W","S","U","S","U"],
-["D","U","D","U","D"],
-["U","S","U","S","U"],
-["D","U","D","U","D"]],]
+    [["D", "U", "D", "U", "D"],
+    ["W", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"],
+    ["U", "S", "U", "S", "U"],
+    ["D", "U", "D", "U", "D"]],]
 
 
 let buildCoaster = function (coaster) {
@@ -72,7 +72,7 @@ let buildCoaster = function (coaster) {
 
         let rowHTML = document.createElement("div")
 
-        rowHTML.classList.add("row", (rowIndex % 2)?"odd" : "even")
+        rowHTML.classList.add("row", (rowIndex % 2) ? "odd" : "even")
 
 
         for (const [spotIndex, spot] of row.entries()) {
@@ -92,6 +92,74 @@ let buildCoaster = function (coaster) {
 
 }
 
-for(let i= 0; i<coasters.length;i+=1){
-document.body.append(buildCoaster(coasters[i]))
+
+let allCoasters = [
+    0,
+    1, 1,
+    2, 2, 2,
+    3, 3, 3, 3,
+    4, 4, 4,
+    5, 5,
+    6
+]
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+
+    // https://stackoverflow.com/a/2450976
 }
+
+let shuffleCoasters = function(coastersArray){
+
+    let shuffledCoasters = shuffle(coastersArray)
+
+    
+    return shuffledCoasters.slice(0,9)
+
+}
+
+
+
+let renderBoard = function(coastersArray){
+
+
+
+    let board = document.createElement("div")
+    board.classList.add("board")
+
+
+    if (coastersArray.length === 9){
+        for(i=0; i<3; i+=1){
+            let boardRow = document.createElement("div")
+            boardRow.classList.add("boardRow")
+
+            for(j=0;j<3;j+=1){
+                let coasterIndex = coastersArray[j*3+i%3]
+               boardRow.append(buildCoaster(coasters[coasterIndex]))
+            }
+            
+            board.append(boardRow)
+        }
+    }
+
+    return board
+
+}
+
+
+
+document.body.append(renderBoard(shuffleCoasters(allCoasters)))
